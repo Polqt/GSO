d723129ac45114e9b72ca2950cbe2421d6fbcf25
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-05-03 00:23:49 +0800

    commit

---
+++ admin-dashboard.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".tickets9");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "admin-tickets.html"; 
+    });
+});
---
+++ admin-tickets.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".dashboard");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "admin-dashboard.html"; 
+    });
+});

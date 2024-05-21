ee4c40a2b18396d7c4eb2d4dfdd9ff5f2d20ddb4
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-05-21 23:08:20 +0800

    commit

--- js/student-dashboard.js
+++ js/student-dashboard.js
@@ -12,6 +12,42 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".notifications22");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-notifications-page.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".messages16");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-messages-inbox-page.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".about-us1");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-about-us.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".help-support1");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-help-support.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".settings24");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-settings.html"; 
+    });
+});
+
+
 document.getElementById("logout").addEventListener("click", redirectToPHP);
 function redirectToPHP() {
     window.location.href = 'index.php';
--- js/student-services.js
+++ js/student-services.js
@@ -33,6 +33,42 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".notifications23");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-notifications-page.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".messages17");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-messages-inbox-page.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".about-us2");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-about-us.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".help-support2");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-help-support.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketsElement = document.querySelector(".settings25");
+    ticketsElement.addEventListener("click", function() {
+        window.location.href = "student-settings.html"; 
+    });
+});
+
+
 document.getElementById("logout").addEventListener("click", redirectToPHP);
 function redirectToPHP() {
     window.location.href = 'index.php';
